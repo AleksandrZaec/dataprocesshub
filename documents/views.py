e5f5e794a76cbc2a4e4eb9d1f4592f459a80a697
AleksandrZaec <zaecam95@mail.ru>
2024-08-08 01:46:10 +0300

    переопределил метод create

--- documents/views.py
+++ documents/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from documents.models import Document
 from documents.permissions import IsAdminGroupOrSuperuserOrReadOnly
 from documents.serializers import DocumentSerializer
@@ -15,6 +17,18 @@ class DocumentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsAdminGroupOrSuperuserOrReadOnly]
 
     def perform_create(self, serializer):
+        """
+            Сохраняет документ и отправляет email-уведомление.
+        """
         document = serializer.save(owner=self.request.user, status='в обработке')
         send_document_creation_email(document, self.request)
+
+    def create(self, request, *args, **kwargs):
+        """
+            Обрабатывает создание нового документа и возвращает кастомное сообщение.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({'message': 'Ваш документ успешно загружен'}, status=status.HTTP_201_CREATED)
 
